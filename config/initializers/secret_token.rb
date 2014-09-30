570fb853ba43e6a76e75402a338492c0ce85bb5a
misunghq <misunghq@misung.biz>
2014-09-30 06:33:23 +0000

    setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Www::Application.config.secret_key_base = 'faccb3c9d2bc3688200249393e8e5954014aaed658f6145eb5fee8bb4d29e92e3b4f3bdba306ea822fd19ffd75fa3794d71894586e736058e8c4f2f6fc4e3523'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Www::Application.routes.draw do
+  root 'welcome/index'
+end
